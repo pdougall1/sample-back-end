b6636562e405439859ad8340b7229a1442d3f9a2
Patrick Dougall <pdougall1@gmail.com>
2015-12-05 16:00:24 -0500

    adds sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  before_filter :authenticate
+
+  def current_user
+    @authenticated_user
+  end
+
+  private
+
+  def authenticate
+    authenticate_user_from_token || render_unauthorized
+  end
+
+  def authenticate_user_from_token
+    if auth_headers = request.headers['Authorization']
+      auth = JSON.parse(auth_headers)
+      identifier = auth['identification']
+      @authenticated_user = identifier && AR::User.where(email: identifier).first
+      @authenticated_user && @authenticated_user.authentication_token.matches_and_valid?(auth['authentication_token'])
+    end
+  end
+
+  def render_unauthorized
+    self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+    render json: {"error" => 'You must provide a valid token'}, status: 401
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+require 'byebug'
+
+class SessionsController < ApplicationController
+  skip_before_filter :authenticate, only: :create
+
+  def create
+    identifier = session_params[:identification]
+    session = Session.create(identifier)
+    render json: session.response
+  end
+
+  def destroy
+    @user = current_user.delete :hashed_authentication_token
+  end
+
+  private
+
+  def session_params
+    params.permit(:password, :identification)
+  end
+end
