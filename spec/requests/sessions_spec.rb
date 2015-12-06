1c9beac9f6607f8cfe1d8657c624acd088ddd4d9
Patrick Dougall <pdougall1@gmail.com>
2015-12-06 13:52:41 -0500

    Adds test that requires a working password

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe 'POST /sessions' do
+  let(:password) { 'password' }
   let(:json) { JSON.parse(response.body) }
   let(:email)   { 'good@example.com' }
-  let(:payload) {{ identification: email }}
+  let(:payload) {{ identification: email, password: password }}
 
   context 'when a user is found with the identification' do
-    let!(:user)   { FactoryGirl.create(:ar_user, email: email) }
+    let!(:user) { FactoryGirl.create(:ar_user, email: email) }
 
     it 'returns the user identification and id' do
       post sessions_path, payload
@@ -14,13 +15,23 @@ describe 'POST /sessions' do
       expect(response.status).to be(200)
       expect(json.keys).to eq(['authentication_token', 'id'])
     end
+
+    context 'but the password does not fit' do
+      let(:encrypted_password) { Auth::Password.new(password) }
+      let!(:user) { FactoryGirl.create(:ar_user, email: email, encrypted_password: encrypted_password) }
+
+      it 'returns a 401' do
+        post sessions_path
+        expect(response.status).to be(404)
+      end
+    end
   end
 
   context 'when a user can not be found' do
-    let(:bad_email)   { 'bad@example.com' }
-    let!(:user)   { FactoryGirl.create(:ar_user, email: bad_email) }
+    let(:bad_email) { 'bad@example.com' }
+    let!(:user) { FactoryGirl.create(:ar_user, email: bad_email) }
 
-    it 'returns the user identification and id' do
+    it 'returns a 404' do
       post sessions_path
       expect(response.status).to be(404)
     end
